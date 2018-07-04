636240bb9567068429c070580fddea4fe571585f
Yoann Lamouroux <yoann.lamouroux@nbs-system.com>
2018-07-04 16:16:05 +0200

    adding paris FR aws region

--- stasto.py
+++ stasto.py
@@ -4,7 +4,7 @@ import boto3
 import click
 
 def validate_region(ctx, param, value):
-    regs = [ 'ap-south-1', 'eu-west-2', 'eu-west-1', 'ap-northeast-2', 'ap-northeast-1', 
+    regs = [ 'ap-south-1', 'eu-west-3', 'eu-west-2', 'eu-west-1', 'ap-northeast-2', 'ap-northeast-1', 
             'sa-east-1', 'ca-central-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1',
             'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2' ]
     if value in regs:
